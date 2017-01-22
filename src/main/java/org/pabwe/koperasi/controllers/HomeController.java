ee48c48df17e9f7dbbba5e8a1d10e6f3de5f02f6
Jaya Haryono Manik <jayaharyonomanik@gmail.com>
2017-01-22 18:22:02 +0700

    Adding index

--- src/main/java/org/pabwe/koperasi/controllers/HomeController.java
+++ src/main/java/org/pabwe/koperasi/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package org.pabwe.koperasi.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class HomeController 
 {
 	@RequestMapping("/")
