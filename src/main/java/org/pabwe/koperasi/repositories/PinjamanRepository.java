f7c2f847a5799d287a35e7a3c87672a0e07e9c4e
Jaya Haryono Manik <jayaharyonomanik@gmail.com>
2017-01-31 23:37:24 +0700

    Get Pinjaman from Id Anggota

--- src/main/java/org/pabwe/koperasi/repositories/PinjamanRepository.java
+++ src/main/java/org/pabwe/koperasi/repositories/PinjamanRepository.java
@@ -4,9 +4,13 @@ import java.io.Serializable;
 
 import org.pabwe.koperasi.models.Pinjaman;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PinjamanRepository extends JpaRepository<Pinjaman, Serializable>{
 
+	@Query("SELECT p FROM Pinjaman p WHERE p.idAnggota = :idAnggota")
+	Pinjaman getPinjamanByIdAnggota(@Param("idAnggota")Integer id);
 }
